55c0663320ad7cf123b8c1a2fef94dd09bcd6c3d
poroxprod <isakov.chrome@gmail.com>
2018-01-10 16:45:46 +0300

    v3

--- site1/urls.py
+++ site1/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     url('about/', about, name='about'),
     url('^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/send/$', contactform, name='contactform'),
 
 
 ]
